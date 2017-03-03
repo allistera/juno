049ee0060d4c4c3395eaa27df51dca5cbfceb376
Allister Antosik <me@allisterantosik.com>
2017-03-03 06:49:58 +0000

    :tada: added authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_225_172_535) do
-  create_table 'checks', force: :cascade do |t|
-    t.integer  'status'
-    t.integer  'site_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['site_id'], name: 'index_checks_on_site_id'
+ActiveRecord::Schema.define(version: 20170303064311) do
+
+  create_table "checks", force: :cascade do |t|
+    t.integer  "status"
+    t.integer  "site_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["site_id"], name: "index_checks_on_site_id"
   end
 
-  create_table 'projects', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'sites', force: :cascade do |t|
-    t.string   'name'
-    t.string   'url'
-    t.integer  'project_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['project_id'], name: 'index_sites_on_project_id'
+  create_table "sites", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_sites_on_project_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
