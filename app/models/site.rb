2288eea320a6a8eaf07a5b3670377165e6d1ed84
Allister Antosik <allistera@gmail.com>
2017-03-09 12:17:50 +0000

    :tada: added active? method to site model (#7)

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,4 +2,9 @@ class Site < ApplicationRecord
   belongs_to :project
   has_many :checks
   validates :name, :url, presence: true
+
+  def active?
+    return false unless checks.last
+    checks.order('created_at').limit(1).first.status.between?(200, 299)
+  end
 end
