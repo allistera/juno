a9a12adb59eed58823c800f0e18e1f9243c23ffe
Allister Antosik <me@allisterantosik.com>
2017-03-27 20:03:45 +0100

    :tada: slack job

--- app/jobs/http_check_job.rb
+++ app/jobs/http_check_job.rb
@@ -10,6 +10,8 @@ class HttpCheckJob < ApplicationJob
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true if uri.scheme == 'https'
 
+      send_alert if newly_failed?
+
       Check.create(site: site,
                    status: call(http, Net::HTTP::Get.new(uri.request_uri)))
     end
@@ -19,5 +21,16 @@ class HttpCheckJob < ApplicationJob
     http.request(request).code
   rescue SocketError
     nil
+  end
+
+  private
+
+  def newly_failed?(site, status)
+    return false if status.between?(200, 299)
+    site.active?
+  end
+
+  def send_alert(site)
+    SlackNotificationJob.perform_later(site)
   end
 end
