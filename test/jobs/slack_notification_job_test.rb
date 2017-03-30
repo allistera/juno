12928e955844f8c180e732abd5e3c041a8c7a837
Allister Antosik <allisterantosik@odin.local>
2017-03-30 14:10:45 +0100

    :tada: styled up slack message

--- test/jobs/slack_notification_job_test.rb
+++ test/jobs/slack_notification_job_test.rb
@@ -2,18 +2,15 @@ require 'test_helper'
 
 class SlackNotificationJobTest < ActiveJob::TestCase
   test 'should send slack alert' do
-    payload = {
-      'channel' => 'bots',
-      'username' => 'MyString',
-      'text' => 'Foo Bar failing to return a successful status code.'
-    }
+    payload = '{"channel":"bots","username":"MyString","text":"\\u003chttp://foo.bar|Foo Bar\\u003e' \
+    ' HTTP check returning 301 status code.","icon_emoji":":exclamation:"}'
     stub_request(:post, 'http://foo.bar/')
-      .with(body: { 'payload' => payload.to_json })
+      .with(body: { 'payload' => payload })
       .to_return(status: 200)
-    SlackNotificationJob.perform_now(sites(:one))
+    SlackNotificationJob.perform_now(sites(:one), 301)
   end
 
   test 'should fail if no slack settings' do
-    refute SlackNotificationJob.perform_now(sites(:two))
+    refute SlackNotificationJob.perform_now(sites(:two), 301)
   end
 end
