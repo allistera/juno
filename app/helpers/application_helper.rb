7073dd67e5f92d86b9207d5f09128c39066f011e
Allister Antosik <allistera@gmail.com>
2017-04-03 13:27:26 +0100

    :tada: added initial unknown site state (#19)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def site_status_class(site)
+    return 'is-light' if site.state == :unknown
+    return 'is-success' if site.state == :active
+    return 'is-danger' if site.state == :inactive
+  end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -8,4 +8,11 @@ class Site < ApplicationRecord
     return false unless last && last.status
     last.status.between?(200, 299)
   end
+
+  def state
+    last = checks.order('created_at desc').limit(1).first
+    return :unknown unless last
+    return :active if last.status.between?(200, 299)
+    return :inactive if last.status < 200 || last.status > 299
+  end
 end
