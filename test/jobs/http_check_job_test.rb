a338091c454822c8abee7926a74a2e5d96c8be29
Allister Antosik <allistera@gmail.com>
2017-04-06 15:09:51 +0100

    :tada: retry once if fails for verification (#27)

--- test/jobs/http_check_job_test.rb
+++ test/jobs/http_check_job_test.rb
@@ -6,5 +6,17 @@ class HttpCheckJobTest < ActiveJob::TestCase
     stub_request(:get, 'http://foo.bar/').to_return(status: 200)
 
     HttpCheckJob.perform_now
+
+    assert_equal 200, Check.last.status
+  end
+
+  test 'should check twice if first fails' do
+    stub_request(:get, 'https://test.com/').to_return(status: 200)
+    stub_request(:get, 'http://foo.bar/').to_return(status: 501)
+    stub_request(:get, 'http://foo.bar/').to_return(status: 501)
+
+    HttpCheckJob.perform_now
+
+    assert_equal 501, Check.last.status
   end
 end
