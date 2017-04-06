38270ff662349c8a951312fe28baf31b31e68687
Allister Antosik <allistera@gmail.com>
2017-04-06 16:34:22 +0100

    :tada: create user create rake task (#28)

---
+++ test/tasks/user_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test 'creates new user account' do
+    assert_difference('User.count') do
+      silenced do
+        Rake::Task['user:create'].invoke('aabcd@gmail.com', '12341212')
+      end
+    end
+  end
+
+  private
+
+  # So we don't get puts to test output
+  def silenced
+    $stdout = StringIO.new
+
+    yield
+  ensure
+    $stdout = STDOUT
+  end
+end
