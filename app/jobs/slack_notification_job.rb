f403d0ed9a057032bc4ce909bf32cc4f84c394af
Allister Antosik <allistera@gmail.com>
2017-04-07 14:23:45 +0100

    :tada: send slack message when site is back up (#29)

--- app/jobs/slack_notification_job.rb
+++ app/jobs/slack_notification_job.rb
@@ -6,9 +6,24 @@ class SlackNotificationJob < ApplicationJob
   def perform(site, status)
     return false unless site.project.slack_setting
 
+    if status.between?(200, 299)
+      active(site, status)
+    else
+      failure(site, status)
+    end
+  end
+
+  def failure(site, status)
     notifier(site).post(
       text: "[#{site.name}](#{site.url}) HTTP check returning #{status} status code.",
       icon_emoji: ':exclamation:'
+    )
+  end
+
+  def active(site, status)
+    notifier(site).post(
+      text: "[#{site.name}](#{site.url}) HTTP check returning #{status} status code.",
+      icon_emoji: ':green_heart:'
     )
   end
 
