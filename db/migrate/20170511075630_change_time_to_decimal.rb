84d459f13570a3a7cae56637555793d3e4fa0acd
Allister Antosik <allister.antosik@aridhia.com>
2017-05-11 10:47:20 +0100

    :bug: Need to cast datatype in postgresql

--- db/migrate/20170511075630_change_time_to_decimal.rb
+++ db/migrate/20170511075630_change_time_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangeTimeToDecimal < ActiveRecord::Migration[5.0]
   def change
-    change_column :checks, :time, :decimal
+    change_column :checks, :time, 'decimal USING CAST(time AS decimal)'
   end
 end
