05b0afc6466b453840dc5d54440e924c662abdb8
Allister Antosik <allister.antosik@aridhia.com>
2017-07-13 13:32:08 +0100

    :tada: dont show users and org menu items if user is not admin

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'user requires organisation' do
-    user = User.new(email: 'foo@bar.com', password: '1110009')
+    user = User.new(email: 'foo@bar.com', password: '1110009', password_confirmation: '1110009')
     refute user.valid?
   end
 
   test 'admin does not require organisation' do
-    user = User.new(email: 'foo@bar.com', password: '1110009', admin: true)
+    user = User.new(email: 'foo@bar.com', password: '1110009', password_confirmation: '1110009', admin: true)
     assert user.valid?
   end
 end
