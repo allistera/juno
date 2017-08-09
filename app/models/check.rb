facc8759d5d1be75174c82359e302e0287464330
Allister Antosik <allistera@gmail.com>
2017-08-09 17:30:59 +0100

    :tada: style last checks table (#83)

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,3 +1,13 @@
 class Check < ApplicationRecord
   belongs_to :site
+
+  def state
+    if site.custom_status && status.to_i == site.custom_status
+      'active'
+    elsif status.to_i.between?(200, 299)
+      'active'
+    else
+      'inactive'
+    end
+  end
 end
--- test/models/check_test.rb
+++ test/models/check_test.rb
@@ -11,4 +11,19 @@ class CheckTest < ActiveSupport::TestCase
     refute check.valid?, 'check is valid without a site'
     assert_not_nil check.errors[:site], 'no validation error for site present'
   end
+
+  test 'state is active' do
+    check = Check.new(status: 201, site: sites(:one))
+    assert_equal 'active', check.state
+  end
+
+  test 'state is active for custom codes' do
+    check = Check.new(status: 301, site: sites(:three))
+    assert_equal 'active', check.state
+  end
+
+  test 'state is inactive' do
+    check = Check.new(status: 501, site: sites(:one))
+    assert_equal 'inactive', check.state
+  end
 end
