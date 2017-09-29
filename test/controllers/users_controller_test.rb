104ddc3ffc2a9ea81f1601818db5b2b72fb438f3
Allister Antosik <allistera@gmail.com>
2017-09-29 22:37:44 +0100

    :tada: allow updating of users as an admin (#100)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,8 +15,33 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       assert_redirected_to new_user_session_url
     end
 
+    it 'rejects non admins' do
+      sign_in users(:joe)
+      get users_url
+      assert_redirected_to root_url
+    end
+
     it 'returns all users' do
       get users_url
+      assert_response :success
+    end
+  end
+
+  describe '#show' do
+    it 'requires authentication' do
+      sign_out :user
+      get user_url(@user)
+      assert_redirected_to new_user_session_url
+    end
+
+    it 'rejects non admins' do
+      sign_in users(:joe)
+      get user_url(@user)
+      assert_redirected_to root_url
+    end
+
+    it 'returns user' do
+      get user_url(@user)
       assert_response :success
     end
   end
@@ -49,6 +74,34 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       assert_template :new
     end
   end
+
+  describe '#update' do
+    it 'requires authentication' do
+      sign_out :user
+      patch user_url(@user)
+      assert_redirected_to new_user_session_url
+    end
+
+    it 'creates update slack setting' do
+      patch user_url(@user), params: {
+        user: {
+          email: 'foo@bar.com',
+          organisation_id: organisations(:peachstones),
+          admin: false
+        }
+      }
+      assert_redirected_to users_url
+    end
+
+    it 'renders edit form on save error' do
+      patch user_url(@user), params: {
+        user: { email: '' }
+      }
+
+      assert_template :show
+    end
+  end
+
   describe '#destroy' do
     it 'requires authentication' do
       sign_out :user
