772685f4920c3d978e6a125f34fec7f0a265688e
Allister Antosik <me@allisterantosik.com>
2017-11-02 19:45:42 +0000

    :white_check_mark: added travisci system test config

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Devise::Test::IntegrationHelpers
+
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   before do
     WebMock.disable!
